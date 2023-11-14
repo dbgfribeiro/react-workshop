c54994e25d453f98a5f11b26d92378238dd66daf
Diogo Ribeiro <diogoribeiro@redlightsoft.com>
2023-11-14 16:50:17 +0000

    renamed components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { NewsCard } from './Components/NewsCard'
+import { NewsCard } from './components/NewsCard'
 import { useState, useEffect } from 'react'
 import { NewsType } from './types/news'
 
